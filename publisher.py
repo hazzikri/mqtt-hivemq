ba4f91a51ece0c72f983877362e12626ef81d347
H. Azzikri <59419033+hazzikri@users.noreply.github.com>
2026-06-22 01:11:00 +0700

    chore: upgrade and complete codebase and documentation

--- publisher.py
+++ publisher.py
@@ -7,12 +7,19 @@ if __name__ == "__main__":
     client.loop_start()
     topic = "test/topic"
     count = 0
-    while True:
-        message = f"Hello MQTT {count}"
-        result = client.publish(topic, message)
-        if result[0] == 0:
-            print(f"Sent `{message}` to `{topic}`")
-        else:
-            print(f"Failed to send message to {topic}")
-        count += 1
-        time.sleep(2)
+    try:
+        while True:
+            message = f"Hello MQTT {count}"
+            result = client.publish(topic, message)
+            status = result[0]
+            if status == 0:
+                print(f"Sent `{message}` to topic `{topic}`")
+            else:
+                print(f"Failed to send message to topic {topic}")
+            count += 1
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("Publisher stopped.")
+    finally:
+        client.loop_stop()
+        client.disconnect()
